c2d3e617e1facf80529614c1c5950f8582e01d91
ragav suriya <surya.ragava@live.com>
2021-06-25 17:41:24 +0530

    checkpoint 4 updated with functional components

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState,useEffect } from 'react';
 import Header from '../../common/header/Header';
 import moviesData from '../../assets/moviesData';
 import Typography from '@material-ui/core/Typography';
@@ -10,55 +10,31 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import { Link } from 'react-router-dom';
 
-class Details extends Component {
-    constructor() {
-        super();
-        this.state = {
-            movie: {},
-            starIcons: [{
-                id: 1,
-                stateId: "star1",
-                color: "black"
-            },
-            {
-                id: 2,
-                stateId: "star2",
-                color: "black"
-            },
-            {
-                id: 3,
-                stateId: "star3",
-                color: "black"
-            },
-            {
-                id: 4,
-                stateId: "star4",
-                color: "black"
-            },
-            {
-                id: 5,
-                stateId: "star5",
-                color: "black"
-            }]
-        }
-    }
+const Details =(props)=>{
+    const [state,setState]=useState({});
+const [ratings,setRatings]=useState([{starIcons:[{id: 1, stateId: "star1",color: "black"},
+                    {id: 2,stateId: "star2",color: "black"},
+                    {id: 3,stateId: "star3",color: "black"},
+                    {id: 4,stateId: "star4",color: "black"},
+                    {id: 5,stateId: "star5",color: "black"}]}]);
+                  
+    let filterMovie=state;  
+useEffect(()=>{
+        
+        filterMovie= moviesData.filter((mov) => {
+            return mov.id === props.match.params.id
+        })[0] ; 
+    setState(filterMovie);
+},[])
 
-    UNSAFE_componentWillMount() {
-        let currentState = this.state;
-        currentState.movie = moviesData.filter((mov) => {
-            return mov.id === this.props.match.params.id
-        })[0];
 
-        this.setState({ currentState });
-    }
-
-    artistClickHandler = (url) => {
+const  artistClickHandler = (url) => {
         window.location = url;
     }
 
-    starClickHandler = (id) => {
+const starClickHandler = (id) => {
         let starIconList = [];
-        for (let star of this.state.starIcons) {
+        for (let star of ratings) {
             let starNode = star;
             if (star.id <= id) {
                 starNode.color = "yellow"
@@ -69,11 +45,9 @@ class Details extends Component {
             }
             starIconList.push(starNode);
         }
-        this.setState({ starIcons: starIconList });
+        setRatings( starIconList );
     }
 
-    render() {
-        let movie = this.state.movie;
         const opts = {
             height: '300',
             width: '700',
@@ -83,7 +57,7 @@ class Details extends Component {
         }
         return (
             <div className="details">
-              <Header id={this.props.match.params.id} showBookShowButton="true" />
+            <Header id={props.match.params.id} showBookShowButton="true" />
                 <div className="back">
                     <Typography>
                         <Link to="/">  &#60; Back to Home</Link>
@@ -91,39 +65,38 @@ class Details extends Component {
                 </div>
                 <div className="flex-containerDetails">
                     <div className="leftDetails">
-                        <img src={movie.poster_url} alt={movie.title} />
+                        <img src={state.poster_url} alt={state.title} />
                     </div>
 
                     <div className="middleDetails">
                         <div>
-                            <Typography variant="h4" component="h2">{movie.title} </Typography>
+                            <Typography variant="h4" component="h2">{state.title} </Typography>
                         </div>
                         <br />
                         <div>
                             <Typography>
-                                <span className="bold">Genres: </span> {movie.genres.join(', ')}
+                                <span className="bold">Genres: </span> {state.genres}
                             </Typography>
                         </div>
                         <div>
-                            <Typography><span className="bold">Duration:</span> {movie.duration} </Typography>
+                            <Typography><span className="bold">Duration:</span> {state.duration} </Typography>
                         </div>
                         <div>
-                            <Typography><span className="bold">Release Date:</span> {new Date(movie.release_date).toDateString()} </Typography>
+                            <Typography><span className="bold">Release Date:</span> {new Date(state.release_date).toDateString()} </Typography>
                         </div>
                         <div>
-                            <Typography><span className="bold"> Rating:</span> {movie.critics_rating}  </Typography>
+                            <Typography><span className="bold"> Rating:</span> {state.critics_rating}  </Typography>
                         </div>
                         <div className="marginTop16">
-                            <Typography><span className="bold">Plot:</span> <a href={movie.wiki_url}>(Wiki Link)</a> {movie.storyline} </Typography>
+                            <Typography><span className="bold">Plot:</span> <a href={state.wiki_url}>(Wiki Link)</a> {state.storyline} </Typography>
                         </div>
                         <div className="trailerContainer">
                             <Typography>
                                 <span className="bold">Trailer:</span>
                             </Typography>
                             <YouTube
-                                videoId={movie.trailer_url.split("?v=")[1]}
+                                videoId={state.vidId}
                                 opts={opts}
-                                onReady={this._onReady}
                             />
                         </div>
                     </div>
@@ -132,11 +105,11 @@ class Details extends Component {
                         <Typography>
                             <span className="bold">Rate this movie: </span>
                         </Typography>
-                        {this.state.starIcons.map(star => (
+                        {ratings.map(star => (
                             <StarBorderIcon
                                 className={star.color}
                                 key={"star" + star.id}
-                                onClick={() => this.starClickHandler(star.id)}
+                                onClick={() => starClickHandler(star.id)}
                             />
                         ))}
 
@@ -147,10 +120,10 @@ class Details extends Component {
                         </div>
                         <div className="paddingRight">
                             <GridList cellHeight={160} cols={2}>
-                                {movie.artists != null && movie.artists.map(artist => (
+                                {state.artists != null && state.artists.map(artist => (
                                     <GridListTile
                                         className="gridTile"
-                                        onClick={() => this.artistClickHandler(artist.wiki_url)}
+                                        onClick={() => artistClickHandler(artist.wiki_url)}
                                         key={artist.id}>
                                         <img src={artist.profile_url} alt={artist.first_name + " " + artist.last_name} />
                                         <GridListTileBar
@@ -165,6 +138,6 @@ class Details extends Component {
             </div>
         )
     }
-}
+
 
 export default Details;
